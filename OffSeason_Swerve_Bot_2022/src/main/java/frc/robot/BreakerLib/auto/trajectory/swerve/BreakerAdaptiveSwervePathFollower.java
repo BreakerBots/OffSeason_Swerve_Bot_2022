d71352ffd876f83bebcdab64fb12b65936bf2bbb
Roman-Tech-Plus <rrcabrahamson@gmail.com>
2022-10-12 00:12:55 -0700

    Update BreakerAdaptiveSwervePathFollower.java

--- OffSeason_Swerve_Bot_2022/src/main/java/frc/robot/BreakerLib/auto/trajectory/swerve/BreakerAdaptiveSwervePathFollower.java
+++ OffSeason_Swerve_Bot_2022/src/main/java/frc/robot/BreakerLib/auto/trajectory/swerve/BreakerAdaptiveSwervePathFollower.java
@@ -4,6 +4,7 @@
 
 // package frc.robot.BreakerLib.auto.trajectory.swerve;
 
+// import java.util.ArrayList;
 // import java.util.List;
 
 // import edu.wpi.first.math.geometry.Pose2d;
@@ -20,17 +21,36 @@
 //     private BreakerInterpolatingTreeMap<Double, BreakerInterpolableDouble> rotInterpolMap;
 //     private BreakerGenericSwerveRotationSupplier rotSupplier;
 //     private List<Translation2d> waypoints;
-    
+//     private List<Translation2d> transTargets;
+//     private List<Pose2d> poseTargets;
+//     private boolean usesPoseTargets, usesSuppliedRotation;
+//     private int legrangeSubdivs;
+
+
 //     public BreakerAdaptiveSwervePathFollower(int legrangeSubdivisions, BreakerGenericSwerveRotationSupplier rotationSupplier, Translation2d... targetPoints) {
 //         rawPoseInterpolMap = new BreakerLegrangeInterpolateingTreeMap<>();
+//         transTargets = new ArrayList<>();
 //         for(Double i = 0.0; i < targetPoints.length; i++) {
 //             rawPoseInterpolMap.put(i, new BreakerInterpolatableDoubleArray(targetPoints[i.intValue()].getX(), targetPoints[i.intValue()].getY()));
 //         }
-        
+//         for(Translation2d tgt: targetPoints) {
+//             transTargets.add(tgt);
+//         }
+//         waypoints = getWaypoints();
         
 //     }
 
 //     public BreakerAdaptiveSwervePathFollower(int legrangeSubdivisions, Pose2d... targetPoses) {
+        
+//     }
 
+//     private List<Translation2d> getWaypoints() {
+//         List<Translation2d> retWp = new ArrayList<>();
+//         double delta = legrangeSubdivs == 0 ? 1.0 : 1.0 / legrangeSubdivs;
+//         for(Double i = 0.0; i < rawPoseInterpolMap.size(); i += delta) {
+//             BreakerInterpolatableDoubleArray dar = i == Math.ceil(i) || i == Math.floor(i) ? rawPoseInterpolMap.get(i) : rawPoseInterpolMap.getInterpolatedValue(i);
+//             retWp.add(new Translation2d(dar.getValue()[0], dar.getValue()[1]));
+//         }
+//         return retWp;
 //     }
 // }
