b88ac37ba3bc0fc7cf336e93e9f045f2f9254de3
Roman-Tech-Plus <rrcabrahamson@gmail.com>
2022-11-09 23:29:04 -0800

    added migrated version folder to proj

---
+++ OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import frc.robot.BreakerLib.devices.sensors.imu.ctre.BreakerPigeon2;
+import frc.robot.BreakerLib.driverstation.BreakerXboxController;
+import frc.robot.BreakerLib.driverstation.BreakerXboxControllerDeadbandConfig;
+import frc.robot.BreakerLib.subsystem.cores.drivetrain.swerve.BreakerSwerveDriveController;
+import frc.robot.BreakerLib.util.power.BreakerPowerManager;
+import frc.robot.BreakerLib.util.robotmanager.BreakerRobotConfig;
+import frc.robot.BreakerLib.util.robotmanager.BreakerRobotManager;
+import frc.robot.BreakerLib.util.robotmanager.BreakerRobotStartConfig;
+import frc.robot.commands.TestTrajectoryAutoPath;
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  private final BreakerXboxController controllerSys = new BreakerXboxController(0);
+  private final BreakerPigeon2 imuSys = new BreakerPigeon2(5);
+  private final Drive drivetrainSys = new Drive(imuSys);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    
+    BreakerRobotManager.setup(
+        drivetrainSys.getBaseDrivetrain(),
+        new BreakerRobotConfig(
+            new BreakerRobotStartConfig(5104, "BreakerBots", "Offseason SwerveBot", 2022, "V1.5",
+                "Roman Abrahamson, and Yousif Alkhalaf")));
+
+    controllerSys.configAnalogInputDeadbands(new BreakerXboxControllerDeadbandConfig(0.06, 0.06, 0.06, 0.06));
+
+    drivetrainSys.getBaseDrivetrain().resetOdometryPosition();
+
+    configureButtonBindings();
+    drivetrainSys.getBaseDrivetrain()
+        .setDefaultCommand(new BreakerSwerveDriveController(drivetrainSys.getBaseDrivetrain(), controllerSys));
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    controllerSys.getButtonB().whenPressed(new InstantCommand(drivetrainSys.getBaseDrivetrain()::toggleSlowMode));
+    controllerSys.getButtonX().whenPressed(new InstantCommand(drivetrainSys.getBaseDrivetrain()::resetOdometryRotation));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new TestTrajectoryAutoPath(drivetrainSys);
+  }
+}
---
+++ OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/commands/TestTrajectoryAutoPath.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.BreakerLib.auto.trajectory.BreakerTrajectoryUtil;
+import frc.robot.BreakerLib.auto.trajectory.management.BreakerStartTrajectoryPath;
+import frc.robot.BreakerLib.auto.trajectory.management.BreakerTrajectoryPath;
+import frc.robot.BreakerLib.auto.trajectory.swerve.BreakerSwerveAutoPathFollower;
+import frc.robot.BreakerLib.auto.trajectory.swerve.BreakerSwerveAutoPathFollowerConfig;
+import frc.robot.BreakerLib.auto.trajectory.swerve.rotation.BreakerRotationPoint;
+import frc.robot.BreakerLib.auto.trajectory.swerve.rotation.BreakerSwerveRotationSupplier;
+import frc.robot.subsystems.Drive;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class TestTrajectoryAutoPath extends SequentialCommandGroup {
+  /** Creates a new TestAutoPath. */
+  public TestTrajectoryAutoPath(Drive drivetrain) {
+    
+    BreakerSwerveAutoPathFollowerConfig swerveFollowerConfig = new BreakerSwerveAutoPathFollowerConfig(drivetrain.getBaseDrivetrain(), 
+        new HolonomicDriveController(new PIDController(2.0, 0.0, 0.1), new PIDController(2.0, 0.0, 0.1), new ProfiledPIDController(0.000000001, 0.0, 0.0, new TrapezoidProfile.Constraints(0.0, 0.0))));
+
+    BreakerTrajectoryPath traj1 = new BreakerTrajectoryPath(TrajectoryGenerator.generateTrajectory(
+        BreakerTrajectoryUtil.toPoseWaypointList(
+          new Pose2d(), 
+          new Pose2d(1.0, 0.0, new Rotation2d()),
+          new Pose2d(1.0, -1.0, new Rotation2d()
+          )),
+        new TrajectoryConfig(0.5, 0.5)), true);
+    
+    addCommands(
+      new BreakerStartTrajectoryPath(drivetrain.getBaseDrivetrain(), new Pose2d()),
+      new BreakerSwerveAutoPathFollower(swerveFollowerConfig, traj1)
+    );
+  }
+}
