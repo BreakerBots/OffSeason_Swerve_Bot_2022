05ad724f64c97ce49b69472fc557bd1229d4b7cd
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-11-24 14:44:16 -0800

    More comments and additions

--- OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/BreakerLib/driverstation/gamepad/components/BreakerAnalogThumbstick.java
+++ OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/BreakerLib/driverstation/gamepad/components/BreakerAnalogThumbstick.java
@@ -4,45 +4,92 @@
 
 package frc.robot.BreakerLib.driverstation.gamepad.components;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.GenericHID;
 
-/** Add your docs here. */
+/** Class which represents an analog HID thumbstick. */
 public class BreakerAnalogThumbstick {
     private GenericHID hid;
     private int xAxisPort, yAxisPort;
     private boolean invertX, invertY;
     private double xDeadband = 0.0, yDeadband = 0.0;
+    private Supplier<Double> xAxisSupplier, yAxisSupplier;
+    
+
+    /**
+     * Constructs an analog thumbstick with desired inverts.
+     * 
+     * @param hid Controller.
+     * @param xAxisPort X-axis port #.
+     * @param yAxisPort Y-axis port #.
+     */
     public BreakerAnalogThumbstick(GenericHID hid, int xAxisPort, int yAxisPort) {
         this(hid, xAxisPort, false, yAxisPort, false);
     }
 
+    /**
+     * Constructs an analog thumbstick with desired inverts.
+     * 
+     * @param hid Controller.
+     * @param xAxisPort X-axis port #.
+     * @param invertX Invert X-axis.
+     * @param yAxisPort Y-axis port #.
+     * @param invertY Invert Y-axis.
+     */
     public BreakerAnalogThumbstick(GenericHID hid, int xAxisPort, boolean invertX, int yAxisPort, boolean invertY) {
         this.hid = hid;
         this.xAxisPort = xAxisPort;
         this.yAxisPort = yAxisPort;
         this.invertX = invertX;
         this.invertY = invertY;
+        xAxisSupplier = () -> getX();
+        yAxisSupplier = () -> getY();
     }
 
+    /** Set stick deadbands.
+     * 
+     * @param xDeadband Deadband for x-axis.
+     * @param yDeadband Deadband for y-axis.
+     */
     public void setDeadband(double xDeadband, double yDeadband) {
         this.xDeadband = xDeadband;
         this.yDeadband = yDeadband;
     }
 
+    /** @return Raw X-axis value. */
     public double getRawX() {
         return hid.getRawAxis(xAxisPort);
     }
 
+    /** @return Raw Y-axis value. */
     public double getRawY() {
         return hid.getRawAxis(yAxisPort);
     }
 
+    /** @return X-axis value. */
     public double getX() {
         return MathUtil.applyDeadband(getRawX(), xDeadband) * (invertX ? -1 : 1);
     }
 
+    /** @return Supplier function which supplies x-axis values. */
+    public Supplier<Double> getXSupplier() {
+        return xAxisSupplier;
+    }
+
+    /** @return Supplier function which supplies y-axis values. */
+    public Supplier<Double> getYSupplier() {
+        return yAxisSupplier;
+    }
+
+    /** @return Y-axis value. */
     public double getY() {
         return MathUtil.applyDeadband(getRawY(), yDeadband) * (invertY ? -1 : 1);
+    }
+
+    /** @return If stick inputs outside of the deadband are detected. */
+    public boolean isActive() {
+        return (getY() != 0 || getX() !=0);
     }
 }
--- OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/BreakerLib/driverstation/gamepad/components/BreakerGamepadThumbstick.java
+++ OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/BreakerLib/driverstation/gamepad/components/BreakerGamepadThumbstick.java
@@ -7,20 +7,23 @@ package frc.robot.BreakerLib.driverstation.gamepad.components;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-/** Add your docs here. */
-public class BreakerGamepadThumbstick extends BreakerAnalogThumbstick{
-    private JoystickButton button;
-    public BreakerGamepadThumbstick(GenericHID hid, int thumbstickButtonPort, int xAxisPort, boolean invertX, int yAxisPort, boolean invertY) {
+/** Class to represent gamepad thumsticks, stick clicks included. */
+public class BreakerGamepadThumbstick extends BreakerAnalogThumbstick {
+    private JoystickButton stickClick;
+
+    /** */
+    public BreakerGamepadThumbstick(GenericHID hid, int thumbstickButtonPort, int xAxisPort, boolean invertX,
+            int yAxisPort, boolean invertY) {
         super(hid, xAxisPort, invertX, yAxisPort, invertY);
-        button = new JoystickButton(hid, thumbstickButtonPort);
+        stickClick = new JoystickButton(hid, thumbstickButtonPort);
     }
 
     public BreakerGamepadThumbstick(GenericHID hid, int thumbstickButtonPort, int xAxisPort, int yAxisPort) {
         super(hid, xAxisPort, yAxisPort);
-        button = new JoystickButton(hid, thumbstickButtonPort);
+        stickClick = new JoystickButton(hid, thumbstickButtonPort);
     }
 
     public JoystickButton getJoystickButton() {
-        return button;
+        return stickClick;
     }
 }
