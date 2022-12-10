c2c8a8d83fe5b115e81c97078d4575595c17ebd5
YousifAlkhalaf <66585473+Roman-Tech-Plus@users.noreply.github.com>
2022-12-10 14:11:11 -0800

    Trying to restructure how robots are constructed. TRUST ME!!!

--- OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/RobotContainer.java
+++ OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/RobotContainer.java
@@ -1,76 +1,122 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Prototyping a new example RobotContainer setup. 
 
 package frc.robot;
 
-import com.ctre.phoenix.CANifier;
+import static frc.robot.Constants.*;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.WPI_CANCoder;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.BreakerLib.devices.sensors.imu.BreakerGenericIMU;
 import frc.robot.BreakerLib.devices.sensors.imu.ctre.BreakerPigeon2;
-import frc.robot.BreakerLib.driverstation.gamepad.buttonbindings.BreakerButtonBinding;
-import frc.robot.BreakerLib.driverstation.gamepad.buttonbindings.BreakerButtonBindingManager;
-import frc.robot.BreakerLib.driverstation.gamepad.buttonbindings.BreakerButtonBindingMap;
-import frc.robot.BreakerLib.driverstation.gamepad.buttonbindings.BreakerButtonBinding.BreakerButtonBindingType;
 import frc.robot.BreakerLib.driverstation.gamepad.components.BreakerGamepadAnalogDeadbandConfig;
 import frc.robot.BreakerLib.driverstation.gamepad.controllers.BreakerXboxController;
+import frc.robot.BreakerLib.subsystem.cores.drivetrain.swerve.BreakerSwerveDrive;
+import frc.robot.BreakerLib.subsystem.cores.drivetrain.swerve.BreakerSwerveDriveConfig;
 import frc.robot.BreakerLib.subsystem.cores.drivetrain.swerve.BreakerTeleopSwerveDriveController;
-import frc.robot.BreakerLib.util.power.BreakerPowerManager;
+import frc.robot.BreakerLib.subsystem.cores.drivetrain.swerve.modules.BreakerMK4iFalconSwerveModule;
+import frc.robot.BreakerLib.util.BreakerArbitraryFeedforwardProvider;
 import frc.robot.BreakerLib.util.robotmanager.BreakerRobotConfig;
 import frc.robot.BreakerLib.util.robotmanager.BreakerRobotManager;
 import frc.robot.BreakerLib.util.robotmanager.BreakerRobotStartConfig;
-import frc.robot.commands.TestTrajectoryAutoPath;
-import frc.robot.subsystems.Drive;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
 
   private final BreakerXboxController controllerSys = new BreakerXboxController(0);
   private final BreakerPigeon2 imuSys = new BreakerPigeon2(5);
-  private final Drive drivetrainSys = new Drive(imuSys);
+  private final BreakerSwerveDrive drivetrainSys = swerveDriveSetup(imuSys);
+  private final BreakerTeleopSwerveDriveController manualDriveCommand = new BreakerTeleopSwerveDriveController(
+      drivetrainSys, controllerSys);
 
-  private final BreakerTeleopSwerveDriveController manualDriveCommand = new BreakerTeleopSwerveDriveController(drivetrainSys.getBaseDrivetrain(), controllerSys);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    // Configure the button bindings
-    
+
     BreakerRobotManager.setup(
-        drivetrainSys.getBaseDrivetrain(),
+        drivetrainSys,
         new BreakerRobotConfig(
             new BreakerRobotStartConfig(5104, "BreakerBots", "Offseason SwerveBot", 2022, "V2.1",
                 "Roman Abrahamson, and Yousif Alkhalaf")));
 
     controllerSys.configDeadbands(new BreakerGamepadAnalogDeadbandConfig(0.06, 0.06, 0.06, 0.06));
 
-    drivetrainSys.getBaseDrivetrain().resetOdometryPosition();
+    drivetrainSys.resetOdometryPosition();
 
     configureButtonBindings();
-    manualDriveCommand.addSlewRateLimiters(new SlewRateLimiter(2.0), new SlewRateLimiter(2.0), new SlewRateLimiter(4.0));
-    drivetrainSys.getBaseDrivetrain().setDefaultCommand(manualDriveCommand);
+    manualDriveCommand.addSlewRateLimiters(new SlewRateLimiter(2.0), new SlewRateLimiter(2.0),
+        new SlewRateLimiter(4.0));
+    drivetrainSys.setDefaultCommand(manualDriveCommand);
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    BreakerButtonBindingManager.addButtonBindingMap(new BreakerButtonBindingMap(
-      new BreakerButtonBinding(controllerSys.getButtonB(), BreakerButtonBindingType.TOGGLE_ON_TRUE, new InstantCommand(drivetrainSys.getBaseDrivetrain()::toggleSlowMode))));
-    controllerSys.getButtonB().toggleOnTrue(new InstantCommand(drivetrainSys.getBaseDrivetrain()::toggleSlowMode));
-    controllerSys.getButtonX().toggleOnTrue(new InstantCommand(drivetrainSys.getBaseDrivetrain()::resetOdometryRotation));
+    controllerSys.getButtonB().toggleOnTrue(new InstantCommand(drivetrainSys::toggleSlowMode));
+    controllerSys.getButtonX()
+        .toggleOnTrue(new InstantCommand(drivetrainSys::resetOdometryRotation));
+  }
+
+  private BreakerSwerveDrive swerveDriveSetup(BreakerGenericIMU imu) {
+
+    var driveFL = new WPI_TalonFX(FL_WHEEL_ID);
+    var turnFL = new WPI_TalonFX(FL_ROTATION_ID);
+    var encoderFL = new WPI_CANCoder(FL_ENCODER_ID);
+
+    var driveFR = new WPI_TalonFX(FR_WHEEL_ID);
+    var turnFR = new WPI_TalonFX(FR_ROTATION_ID);
+    var encoderFR = new WPI_CANCoder(FR_ENCODER_ID);
+
+    var driveBL = new WPI_TalonFX(BL_WHEEL_ID);
+    var turnBL = new WPI_TalonFX(BL_ROTATION_ID);
+    var encoderBL = new WPI_CANCoder(BL_ENCODER_ID);
+
+    var driveBR = new WPI_TalonFX(BR_WHEEL_ID);
+    var turnBR = new WPI_TalonFX(BR_ROTATION_ID);
+    var encoderBR = new WPI_CANCoder(BR_ENCODER_ID);
+
+    BreakerSwerveDriveConfig config = new BreakerSwerveDriveConfig(
+        4.1148, 4.1148, 16.1148,
+        1.25, 0.0, 0.05,
+        0.35, 0.0, 0.0, 0.0,
+        8.14, 4.0, 0.001, 4.1148,
+        new BreakerArbitraryFeedforwardProvider(2.75, 0.2),
+        FL_TRANSLATION, FR_TRANSLATION, BL_TRANSLATION, BR_TRANSLATION);
+    config.setSlowModeMultipliers(0.5, 0.5);
+
+    var frontLeftModule = new BreakerMK4iFalconSwerveModule(driveFL, turnFL, encoderFL, config, 121, true, true);
+    frontLeftModule.setDeviceName(" FL_Module ");
+
+    var frontRightModule = new BreakerMK4iFalconSwerveModule(driveFR, turnFR, encoderFR, config, -61, false, true);
+    frontRightModule.setDeviceName(" FR_Module ");
+
+    var backLeftModule = new BreakerMK4iFalconSwerveModule(driveBL, turnBL, encoderBL, config, 30.0, true, true);
+    backLeftModule.setDeviceName(" BL_Module ");
+
+    var backRightModule = new BreakerMK4iFalconSwerveModule(driveBR, turnBR, encoderBR, config, -176.0, false, true);
+    backRightModule.setDeviceName(" BR_Module ");
+
+    return new BreakerSwerveDrive(config, imu, frontLeftModule, frontRightModule, backLeftModule, backRightModule);
   }
 
   /**
@@ -79,6 +125,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return drivetrainSys.getBaseDrivetrain().getTestSuite().stressTest(4.0, 4.0, 16.0);
+    return drivetrainSys.getTestSuite().stressTest(4.0, 4.0, 16.0);
   }
 }
