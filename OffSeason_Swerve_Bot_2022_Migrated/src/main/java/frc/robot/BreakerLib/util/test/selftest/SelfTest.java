71a40fe8c9071e59afca3c3f4427f0ef6dd63ca2
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-12-16 23:00:59 -0800

    I want to test something with SelfTest...

--- OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/BreakerLib/util/test/selftest/SelfTest.java
+++ OffSeason_Swerve_Bot_2022_Migrated/src/main/java/frc/robot/BreakerLib/util/test/selftest/SelfTest.java
@@ -6,7 +6,11 @@ package frc.robot.BreakerLib.util.test.selftest;
 
 import java.util.ArrayList;
 import java.util.List;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.BreakerLib.devices.cosmetic.music.BreakerFalconOrchestra;
 import frc.robot.BreakerLib.devices.cosmetic.music.BreakerSounds;
 import frc.robot.BreakerLib.util.logging.BreakerLog;
@@ -18,85 +22,137 @@ import frc.robot.BreakerLib.util.logging.BreakerLog;
  * {@link BreakerSelfTestable} compatible devices, or devices registered
  * through an instance of the {@link SystemDiagnostics} class.
  */
-public class SelfTest extends SubsystemBase {
-  private int cycleCount;
-  private static String lastSystemCheck;
-  private static List<BreakerSelfTestable> devices = new ArrayList<BreakerSelfTestable>();
-  private static int cyclesbetweenPerSelfCecks = 250;
-  private static boolean lastCheckPassed = true;
+public class SelfTest{
+
+  private static int cycleCount;
+  private static String lastSystemTest;
+  private static List<BreakerSelfTestable> deviceList = new ArrayList<BreakerSelfTestable>();
+
+  /** # of cycles between self-tests. For accurate conversion, seconds must be divisible by time per cycle (usually 20ms) */
+  private static int cyclesBetweenSelfTests = 250;
+  private static boolean lastTestPassed = true;
   private static BreakerFalconOrchestra orchestra;
   private static boolean usesOrchestra = false;
-  private static boolean autoRegesterDevices = true;
+  private static boolean autoRegisterDevices = true;
   private static boolean selfTestEnabled = true;
 
-  /** Configures an enables a SelfTest check cycle */
-  public SelfTest(double secondsBetweenPeriodicSelfChecks) {
-    SelfTest.cyclesbetweenPerSelfCecks = (int) (secondsBetweenPeriodicSelfChecks * 50);
+  /** Runs self-test loop only when enough time has passed. Otherwise schedules a null command. */
+  private static ConditionalCommand selfTestLoop = new ConditionalCommand(new InstantCommand(() -> runSelfTest()), new WaitCommand(0), () -> enoughCyclesPassed());
+
+  /**
+   * Configures and enables a SelfTest cycle. Devices automatically are
+   * registered.
+   * 
+   * @param secondsBetweenSelfTests # of seconds between self-tests.
+   */
+  public SelfTest(double secondsBetweenSelfTests) {
+    SelfTest.cyclesBetweenSelfTests = (int) (secondsBetweenSelfTests * 50);
     SelfTest.usesOrchestra = false;
-    SelfTest.autoRegesterDevices = true;
+    SelfTest.autoRegisterDevices = true;
+    startPeriodicTests();
   }
 
-  /** Configures an enables a SelfTest check cycle */
-  public SelfTest(double secondsBetweenPeriodicSelfChecks, boolean autoRegesterDevices) {
-    SelfTest.cyclesbetweenPerSelfCecks = (int) (secondsBetweenPeriodicSelfChecks * 50);
+  /**
+   * Configures and enables a SelfTest cycle.
+   * 
+   * @param secondsBetweenPeriodicSelftests # of seconds between self-tests.
+   * @param autoRegisterDevices              Whether or not devices will
+   *                                         automatically register.
+   */
+  public SelfTest(double secondsBetweenPeriodicSelftests, boolean autoRegisterDevices) {
+    SelfTest.cyclesBetweenSelfTests = (int) (secondsBetweenPeriodicSelftests * 50);
     SelfTest.usesOrchestra = false;
-    SelfTest.autoRegesterDevices = autoRegesterDevices;
+    SelfTest.autoRegisterDevices = autoRegisterDevices;
+    startPeriodicTests();
   }
 
-  /** Configures an enables a SelfTest check cycle */
-  public SelfTest(double secondsBetweenPeriodicSelfChecks, BreakerFalconOrchestra orchestra) {
-    SelfTest.cyclesbetweenPerSelfCecks = (int) (secondsBetweenPeriodicSelfChecks * 50);
+  /**
+   * Configures and enables a SelfTest cycle. Devices automatically are
+   * registered.
+   * 
+   * @param secondsBetweenPeriodicSelftests # of seconds between self-tests.
+   * @param orchestra                        Falcon Orchestra to use.
+   */
+  public SelfTest(double secondsBetweenPeriodicSelftests, BreakerFalconOrchestra orchestra) {
+    SelfTest.cyclesBetweenSelfTests = (int) (secondsBetweenPeriodicSelftests * 50);
     SelfTest.orchestra = orchestra;
     SelfTest.usesOrchestra = true;
+    startPeriodicTests();
   }
 
-  /** Configures an enables a SelfTest check cycle */
-  public SelfTest(double secondsBetweenPeriodicSelfChecks, BreakerFalconOrchestra orchestra,
-      boolean autoRegesterDevices) {
-    SelfTest.cyclesbetweenPerSelfCecks = (int) (secondsBetweenPeriodicSelfChecks * 50);
+  /**
+   * Full constructor for SelfTest.
+   * 
+   * @param secondsBetweenPeriodicSelftests # of seconds between self-tests.
+   * @param orchestra                        Falcon Orchestra to use.
+   * @param autoRegisterDevices              Whether or not devices will
+   *                                         automatically register.
+   */
+  public SelfTest(double secondsBetweenPeriodicSelftests, BreakerFalconOrchestra orchestra,
+      boolean autoRegisterDevices) {
+    SelfTest.cyclesBetweenSelfTests = (int) (secondsBetweenPeriodicSelftests * 50);
     SelfTest.orchestra = orchestra;
     SelfTest.usesOrchestra = true;
-    SelfTest.autoRegesterDevices = autoRegesterDevices;
+    SelfTest.autoRegisterDevices = autoRegisterDevices;
+    startPeriodicTests();
   }
 
-  /** Enables or dissables the periodic self test diagnstic check cycle */
+  /** Schedules the command that will run the self-test loop. */
+  public static void startPeriodicTests() {
+    CommandScheduler.getInstance().schedule(selfTestLoop);
+  }
+
+  /** @return If enough cycles have passed to run another self-test. */
+  public static boolean enoughCyclesPassed() {
+    return cycleCount++ % cyclesBetweenSelfTests == 0;
+  }
+
+  /** Enables or disables the periodic self test diagnstic test cycle. */
   public static void setSelfTestEnabled(boolean isEnabled) {
     selfTestEnabled = isEnabled;
   }
 
-  /** @return if the periodic self test diagnstic check cycle is enabled */
+  /** @return If the periodic self test diagnstic test cycle is enabled. */
   public static boolean isSelfTestEnabled() {
     return selfTestEnabled;
   }
 
   /**
-   * Automaticly adds a {@link BreakerSelfTestable} compatable device to the
-   * SelfTest queue if automatic registration is enabled.
-   * <br>
-   * <br>
+   * Automatically adds given {@link BreakerSelfTestable} compatible devices to
+   * the SelfTest queue if automatic registration is enabled.
+   * <p>
+   * <b>
    * WARNING: should only be used in BreakerLib internal classes and is toggled
-   * based on user config of selftest.
+   * based on user config of Self-Test.
+   * <b>
+   * 
+   * @param device Device to add.
    */
-  public static void autoRegisterDevice(BreakerSelfTestable device) {
-    if (autoRegesterDevices) {
-      devices.add(device);
+  public static void autoRegisterDevices(BreakerSelfTestable device) {
+    if (autoRegisterDevices) {
+      addDevices(device);
     }
   }
 
   /**
-   * Automaticly adds multipul {@link BreakerSelfTestable} compatable devices to
+   * Automatically adds given {@link BreakerSelfTestable} compatible devices to
    * the SelfTest queue if automatic registration is enabled.
-   * <br>
-   * <br>
-   * WARNING: should only be used in BreakerLib internal classes and is toggled
-   * based on user config of selftest.
+   * <p>
+   * <b>
+   * WARNING: This should only be used in BreakerLib internal classes and is
+   * toggled
+   * based on user config of Self-Test.
+   * <b>
+   * 
+   * @param devices Devices to automatically be added.
    */
-  public static void autoRegisterDevices(BreakerSelfTestable... devices) {
-    if (autoRegesterDevices) {
+  protected static void autoRegisterDevices(BreakerSelfTestable... devices) {
+    if (autoRegisterDevices) {
       addDevices(devices);
     }
   }
 
+  /** If orchestra is in use, alarm will play. */
   private static void runAlarm() {
     if (usesOrchestra) {
       orchestra.startLoopedSong(BreakerSounds.GeneralAlarmSound);
@@ -104,89 +160,73 @@ public class SelfTest extends SubsystemBase {
   }
 
   /**
-   * Maunualy adds a {@link BreakerSelfTestable} compatable device to the SelfTest
-   * queue.
-   * <br>
-   * <br>
-   * WARNING: If this is done to a device that has allready been added
-   * (including automatic additions, if enabled), will be checked twice,
-   * possably with signifcant runtime expence
-   */
-  public static void addDevice(BreakerSelfTestable device) {
-    devices.add(device);
-  }
-
-  /**
-   * Maunualy adds multipul {@link BreakerSelfTestable} compatable devices to the
+   * Manually adds given {@link BreakerSelfTestable} compatible devices to the
    * SelfTest queue.
-   * <br>
-   * <br>
+   * <p>
+   * <b>
    * WARNING: If this is done to a device that has allready been added
-   * (including automatic additions, if enabled), will be checked twice,
+   * (including automatic additions, if enabled), will be tested twice,
    * possably with signifcant runtime expence
+   * <b>
+   * 
+   * @param devicesToAdd Devices to be added to queue.
    */
   public static void addDevices(BreakerSelfTestable... devicesToAdd) {
-    for (BreakerSelfTestable div : devicesToAdd) {
-      devices.add(div);
+    for (BreakerSelfTestable device : devicesToAdd) {
+      deviceList.add(device);
     }
   }
 
   /**
-   * @return The logged fault string result of the most recent self check cycle
+   * @return The logged fault string result of the most recent self test cycle.
    */
-  public static String getLastSelfCheck() {
-    return lastSystemCheck;
+  public static String getLastSelftest() {
+    return lastSystemTest;
   }
 
   /**
-   * @return True if the last self check was passes without a fault, false
-   *         outherwise
+   * @return True if the last self test passed without fault, false
+   *         otherwise.
    */
-  public static boolean getLastSelfCheckPassed() {
-    return lastCheckPassed;
+  public static boolean lastSelfTestPassed() {
+    return lastTestPassed;
   }
 
   /**
-   * @return True if the {@link SelfTest#autoRegisterDevice()} and
-   *         {@link SelfTest#autoRegisterDevices()} are enabled by the user, flase
-   *         outherwise
+   * @return True if {@link SelfTest#autoRegisterDevice()} and
+   *         {@link SelfTest#autoRegisterDevices()} are enabled by the user, false
+   *         otherwise.
    */
   public static boolean getAutoRegesterDevicesIsEnabled() {
-    return autoRegesterDevices;
+    return autoRegisterDevices;
   }
 
   /**
-   * Method that runs this robots Self Check cycle, pereodicly called by this
-   * classes pereodic method
+   * Self-Test loop method. Automatically called periodically when SelfTest object is constructed.
    */
-  public static void runSelfCheck() {
-    StringBuilder work = new StringBuilder("\n RUNNING SELF CHECK: \n");
+  public static void runSelfTest() {
+    StringBuilder work = new StringBuilder("\n RUNNING SELF test: \n");
     List<BreakerSelfTestable> faultDevices = new ArrayList<BreakerSelfTestable>();
-    for (BreakerSelfTestable device : devices) {
+
+    for (BreakerSelfTestable device : deviceList) {
       device.runSelfTest();
       if (device.hasFault()) {
         faultDevices.add(device);
       }
     }
-    if (!faultDevices.isEmpty()) {
-      work.append(" SELF CHECK FAILED - FAULTS FOUND: \n");
-      lastCheckPassed = false;
-      for (BreakerSelfTestable faultDiv : faultDevices) {
-        work.append(" | " + faultDiv.getDeviceName() + "-" + faultDiv.getFaults() + " | ");
-      }
-      runAlarm();
-    } else {
-      work.append(" SELF CHECK PASSED ");
-      lastCheckPassed = true;
-    }
-    lastSystemCheck = work.toString();
-    BreakerLog.log(lastSystemCheck);
-  }
 
-  @Override
-  public void periodic() {
-    if ((cycleCount++ % cyclesbetweenPerSelfCecks == 0) && selfTestEnabled) {
-      runSelfCheck();
+    if (!faultDevices.isEmpty()) {
+      work.append(" SELF test FAILED - FAULTS FOUND: \n");
+      lastTestPassed = false;
+      for (BreakerSelfTestable faultyDevice : faultDevices) {
+        work.append(" | " + faultyDevice.getDeviceName() + "-" + faultyDevice.getFaults() + " | ");
+      }
+      runAlarm(); // Only works if Orchestra is enabled.
+    } else {
+      work.append(" SELF test PASSED ");
+      lastTestPassed = true;
     }
+    lastSystemTest = work.toString();
+    BreakerLog.log(lastSystemTest);
   }
 }
